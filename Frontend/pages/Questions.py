6048f40c3229a1ef268f006072e48b22d8a83829
Rooij,Tijn T. de <t.derooij@student.fontys.nl>
2024-06-03 10:32:15 +0200

    On submit added

--- Frontend/pages/Questions.py
+++ Frontend/pages/Questions.py
@@ -7,13 +7,19 @@ def get_all_beer_names():
 def setBeerRecommendation(beer):
     st.session_state.beer = beer
 
+def on_submit(car, beers):
+    st.session_state.car = car
+    st.session_state.top_beers = beers
+
+if "submitted_questions" not in st.session_state:
+    st.session_state.submitted_questions = False
 
 st.header("Please answer these questions for a better result")
 with st.container():
     with st.form("Questions_form"):
-        st.write("What are your top beers?")
+        st.write("What are your top 3 beers?")
 
-        options = st.multiselect(
+        beers = st.multiselect(
             label="Choose beers",
             options=get_all_beer_names())
         
@@ -23,4 +29,9 @@ with st.container():
                         captions = ['Drive save', 'Drink save'])
 
         # Every form must have a submit button.
-        st.form_submit_button("Submit")
+        st.session_state.submitted_questions = st.form_submit_button("Submit")
+
+        if st.session_state.submitted_questions and len(beers) >= 3:
+            on_submit(car, beers)
+        else:
+            st.error('Please submit more beer for a better result')
